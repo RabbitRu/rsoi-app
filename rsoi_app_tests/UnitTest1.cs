b926eed2c2b110e9eb0799c33d239c29d409c492
Егор Домнин <edomnin@ivi.ru>
2018-11-11 01:17:37 +0300

    Добавил тесты

---
+++ rsoi_app_tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace rsoi_app_tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.Write("Test 1 OK!");
+            return;
+        }
+    }
+}
